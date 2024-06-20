3b90ee26d7674f56ea5d009c00b2a61f1897195a
antonborisoff <antonborisoff@mail.ru>
2024-06-20 11:26:01 +0300

    copy local server from contracts-angular (#5)

---
+++ src/contracts.ts
@@ -0,0 +1,32 @@
+export const CONTRACTS = [
+  {
+    id: 'contract_1',
+    number: 'APX100',
+    conditions: 'one year labour contract'
+  },
+  {
+    id: 'contract_2',
+    number: 'APX200',
+    conditions: 'two year labour contract'
+  },
+  {
+    id: 'contract_3',
+    number: 'APX300',
+    conditions: 'three year labour contract'
+  },
+  {
+    id: 'contract_4',
+    number: 'APX400',
+    conditions: 'four year labour contract'
+  },
+  {
+    id: 'contract_5',
+    number: 'APX500',
+    conditions: 'five year labour contract'
+  },
+  {
+    id: 'contract_6',
+    number: 'APX600',
+    conditions: 'six year labour contract'
+  }
+]
